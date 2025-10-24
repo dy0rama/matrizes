53ecfa265e93f5f0b56259b0cf42efbd2d2ecce6
dy0rama <rodrigomvianaart@gmail.com>
2025-10-24 03:34:28 -0300

    primeiro commit

---
+++ src/MatrizExercicio.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MatrizExercicio {
+    static void main(){
+        Scanner sc = new Scanner(System.in);
+        int[][] numeros = new int[3][3];
+
+        for (int i = 0; i < numeros.length; i++){
+            for (int j = 0; j < numeros[i].length; j++){
+                System.out.print("Digite o valor da linha " + i + " coluna " + j + ": ");
+                numeros[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < numeros.length; i++){
+            for (int j = 0; j < numeros[i].length; j++) {
+                System.out.print(numeros[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
